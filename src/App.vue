eabb56d4fd90623da62bdf033881e1286bb56f12
Lee min jeong <helloyimj@gmail.com>
2022-04-05 14:51:06 +0900

    comp0405

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <h1 @click="increase">
+    {{ count }}
+  </h1>
+  <div v-if="count > 4">
+    4보다 큽니다!
+  </div>
+  <ul>
+    <Fruit
+      v-for="fruit in fruits"
+      :key="fruit"
+      :name="fruit">
+      {{ fruit }}
+    </Fruit>
+  </ul>
+</template>
+
+<script>
+import Fruit from '~/comments/Fruit'
+
+export default {
+  components: {
+    Fruit
+    // Fruit: Fruit // 객체 데이터 내부의 속성의 이름과 데이터의 이름이 같으면 생략 가능
+  },
+  data() {
+    return {
+      count: 0,
+      fruits: ['Apple', 'Banana', 'Cherry']
+    }
+  },
+  methods: {
+    increase() {
+      this.count += 1
+    }
+  }
+
+}
+</script>
+
+<style lang="scss">
+h1 {
+  font-size: 50px;
+  color: royalblue;
+}
+ul {
+  li {
+    font-size: 40px;
+  }
+}
+</style>
---
+++ src/components/Fruits.vue
@@ -0,0 +1,21 @@
+<template>
+  <li>{{ name }}?!</li>
+</template>
+
+<script>
+export default {
+	props: {
+		name: {
+			type: String,
+			default: ''
+		}
+	}
+}
+</script>
+
+
+<style scoped lang="scss">
+h1 {
+color: red !important;
+}
+</style>
